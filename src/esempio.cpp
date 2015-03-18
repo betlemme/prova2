88b465ede4e2769e54802163cadbc367e702c75f
betlemme <tonezza@live.it>
2015-03-18 11:19:45 +0100

    puntatori

--- src/esempio.cpp
+++ src/esempio.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
 main()
 {
-    std::cout << "Hello World!" <<std::endl;
+    int a = 4;
+    int b = 77;
+
+    int *p = &a;
+
+    cout << "a: " << a << endl;
+    cout << "&a: " << &a << endl;
+    cout << "p:  " << p << endl;
+    cout << "&p: " << &p << endl;
+    cout << "*p: " << *p << endl;
+
     return 0;
 }
